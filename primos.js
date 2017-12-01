7149072832a0a93b916299fa7407cbfcf50afaaa
Your Name <you@example.com>
2017-12-01 14:49:45 +0100

    commit1

---
+++ primos.js
@@ -0,0 +1,14 @@
+
+function esprimo(numero) {
+    var i = 2
+    var numero 
+    while (i<numero) {
+        if (numero%i==0) {
+            esprimo = false;
+        i++
+        } else {
+            esprimo = true;
+        }
+    return esprimo;
+    }
+}
